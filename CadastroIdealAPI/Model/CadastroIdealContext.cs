4c127ebb3a909618304b9017aa8870e3327fc5b1
Fabio Dos Santos <fabio2st@yahoo.com>
2021-02-10 19:21:07 -0300

    Model & context

---
+++ CadastroIdealAPI/Model/CadastroIdealContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroIdealAPI.Model
+{
+    public class CadastroIdealContext : DbContext
+    {
+        public CadastroIdealContext(DbContextOptions<CadastroIdealContext> options) : base(options)
+        { 
+        }
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
